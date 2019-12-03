f00fc9117dad63955ae514a900b729e836464f32
gabrielSBorges <borgesgabrielsilva@gmail.com>
2019-12-03 19:27:03 -0300

    Parte front end terminada

--- coldigoGeladeiras/WebContent/js/admin.js
+++ coldigoGeladeiras/WebContent/js/admin.js
@@ -4,6 +4,28 @@ $(document).ready(() => {
     $("header").load("/coldigoGeladeiras/pages/admin/general/header.html")
     $("footer").load("/coldigoGeladeiras/pages/admin/general/footer.html")
 
+    COLDIGO.empty = (value) => {
+        if (value == 'undefined' || value == undefined || typeof value == 'undefined') {
+            return true
+        }
+        
+        if (value == "") {
+            return true
+        }    
+
+        if (value.length < 1) {
+            return true
+        }
+
+        if (!isNaN(value)) {
+            if (value == 0 || value == "0" || value == 0.0 || value == "0.0" || value == 0.00 || value == "0.00") {
+                return true
+            }
+        }
+
+        return false
+    }
+
     COLDIGO.carregaPagina = (pagename) => {
         $("section").empty()
         $("section").load(pagename + "/", (response, status, info) => {
@@ -12,5 +34,22 @@ $(document).ready(() => {
                 $("section").html(msg)
             }
         })
+    }
+
+    COLDIGO.exibirAviso = (aviso) => {
+        let modal = {
+            title: "Mensagem",
+            height: 250,
+            width: 400,
+            modal: true,
+            buttons: {
+                "OK": function() {
+                    $(this).dialog("close")
+                }
+            }
+        }
+
+        $("#modalAviso").html(aviso)
+        $("#modalAviso").dialog(modal)
     }
 })
--- coldigoGeladeiras/WebContent/js/product.js
+++ coldigoGeladeiras/WebContent/js/product.js
@@ -2,16 +2,43 @@ COLDIGO.produto = new Object()
 
 $(document).ready(function() {
     COLDIGO.produto.carregarMarcas = function() {
-        alert("Tentando buscar marcas")
         $.ajax({
             type: "GET",
             url: "/coldigoGeladeiras/rest/marca/buscar",
             success: function(marcas) {
-                alert("Sucesso")
-                console.log(marcas)
+                if (!COLDIGO.empty(marcas)) {
+                    $("#selMarca").html("")
+
+                    let option = document.createElement("option")
+                    option.setAttribute("value", "")
+                    option.innerHTML = ("Escolha")
+                    $("#selMarca").append(option)
+
+                    for (const i in marcas) {
+                        let option = document.createElement("option")
+                        option.setAttribute("value", marcas[i].id)
+                        option.innerHTML = (marcas[i].nome)
+                        $("#selMarca").append(option)
+                    }
+                } else {
+                    $("#selMarca").html("")
+
+                    let option = document.createElement("option")
+                    option.setAttribute("value", "")
+                    option.innerHTML = ("Cadastre uma marca primeiro!")
+                    $("#selMarca").append(option)
+                    $("#selMarca").addClass("aviso")
+                }
             },
             error: function(info) {
-                alert("Erro")
+                COLDIGO.exibirAviso("Erro ao buscar as marcas: " + info.status + " - " + info.statusText)
+
+                $("#selMarca").html("")
+                let option = document.createElement("option")
+                option.setAttribute("value", "")
+                option.innerHTML = ("Erro ao carregar marcas!")
+                $("#selMarca").append(option)
+                $("#selMarca").addClass("aviso")
             }
         })
     }
